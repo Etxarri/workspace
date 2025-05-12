9a5e969dd12e765d05a692e8680cc4d65b3752ef
aitzolmoreno <aitzol.moreno@alumni.mondragon.edu>
2025-05-12 14:53:45 +0000

    Quitar StringAplication

--- src/main/java/edu/mondragon/webengl/spring_kaixo_mundua/SpringKaixoMunduaApplication.java
+++ src/main/java/edu/mondragon/webengl/spring_kaixo_mundua/SpringKaixoMunduaApplication.java
@@ -8,9 +8,6 @@ public class SpringKaixoMunduaApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(SpringKaixoMunduaApplication.class, args);
-		SpringApplication.run(SpringKaixoMunduaApplication.class, args);
-		SpringApplication.run(SpringKaixoMunduaApplication.class, args);
-		SpringApplication.run(SpringKaixoMunduaApplication.class, args);
 	}
 
 }
