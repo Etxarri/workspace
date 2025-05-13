9327beb280fc4bf405705c0216bd21d3a97d08f7
aitzolmoreno <aitzol.moreno@alumni.mondragon.edu>
2025-05-13 13:49:26 +0000

    HelloWorldSpring

--- src/main/java/edu/mondragon/webengl/controller/hellocontroller.java
+++ src/main/java/edu/mondragon/webengl/controller/hellocontroller.java
@@ -1,15 +1,17 @@
 package edu.mondragon.webengl.controller;
 
-
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// @Controller
+// @ResponseBody
 @RestController
 public class HelloController {
 
-    @GetMapping("/")
-    public String kaixo() {
-        return "Kaixo Mundua!";
+    @RequestMapping("/")
+    String hello() {
+        return "Hello World, Spring Boot!";
     }
+
 }
 
