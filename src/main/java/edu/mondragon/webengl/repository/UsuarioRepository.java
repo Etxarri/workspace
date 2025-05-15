9150926e9b899895f5d5c47a1a55fece94d00656
aitzolmoreno <aitzol.moreno@alumni.mondragon.edu>
2025-05-15 15:04:14 +0000

    WebAplicationSpringII

--- src/main/java/edu/mondragon/webengl/repository/UsuarioRepository.java
+++
@@ -1,7 +0,0 @@
-package edu.mondragon.webengl.repository;
-
-
-import edu.mondragon.webengl.model.Usuario;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UsuarioRepository extends JpaRepository<Usuario, Long> {}
