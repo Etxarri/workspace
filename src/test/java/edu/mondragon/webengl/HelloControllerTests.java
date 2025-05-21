48702042d02125b052dc9e8783842439bf742513
Mikel Etxarri <mikel.echarri@alumni.mondragon.edu>
2025-05-21 15:56:03 +0200

    correo

--- src/test/java/edu/mondragon/webengl/HelloControllerTests.java
+++ src/test/java/edu/mondragon/webengl/HelloControllerTests.java
@@ -1,6 +1,5 @@
 package edu.mondragon.webengl;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,6 +10,7 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import static org.hamcrest.Matchers.equalTo;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import org.junit.Test;
 
 @SpringBootTest
 @AutoConfigureMockMvc
