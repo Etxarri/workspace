34301c898033bae4896d5ad94f2f1870568e6ba6
aitzolmoreno <aitzol.moreno@alumni.mondragon.edu>
2025-06-09 13:54:00 +0000

    METER LO DE IBON BIEN

---
+++ src/main/java/edu/mondragon/webengl/controller/ChatWebSocketController.java
@@ -0,0 +1,48 @@
+package edu.mondragon.webengl.controller;
+
+import java.time.LocalDateTime;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Controller;
+import edu.mondragon.webengl.domain.chat.model.MensajeChat;
+import edu.mondragon.webengl.domain.chat.model.MensajeChatDTO;
+import edu.mondragon.webengl.domain.chat.service.MensajeChatService;
+import edu.mondragon.webengl.domain.user.model.Usuario;
+import edu.mondragon.webengl.domain.user.repository.UsuarioRepository;
+
+@Controller
+public class ChatWebSocketController {
+
+    @Autowired
+    private UsuarioRepository usuarioRepo;
+
+    @Autowired
+    private MensajeChatService chatService;
+
+    @Autowired
+    private SimpMessagingTemplate messagingTemplate;
+
+    @MessageMapping("/chat/{idioma}/mensaje")
+    public void enviarMensaje(@DestinationVariable String idioma, MensajeChatDTO dto) {
+        Usuario usuario = usuarioRepo.findById(dto.getUsuarioID()).orElseThrow();
+
+        MensajeChat mensaje = new MensajeChat();
+        mensaje.setUsuario(usuario);
+        mensaje.setContenido(dto.getContenido());
+        mensaje.setFechaHora(LocalDateTime.now());
+        mensaje.setIdioma(idioma);
+
+        MensajeChat guardado = chatService.guardarMensaje(mensaje);
+
+        MensajeChatDTO respuesta = new MensajeChatDTO();
+        respuesta.setUsuarioID(usuario.getUsuarioID());
+        respuesta.setNombre(usuario.getNombre());
+        respuesta.setContenido(guardado.getContenido());
+        respuesta.setFechaHora(guardado.getFechaHora());
+        respuesta.setIdioma(idioma);
+
+        messagingTemplate.convertAndSend("/topic/chat/" + idioma, respuesta);
+    }
+}
---
+++ src/main/java/edu/mondragon/webengl/domain/chat/model/MensajeChatDTO.java
@@ -0,0 +1,58 @@
+package edu.mondragon.webengl.domain.chat.model;
+
+import java.time.LocalDateTime;
+
+public class MensajeChatDTO {
+
+    private Integer usuarioID;
+    private String nombre;
+    private String contenido;
+    private LocalDateTime fechaHora;
+    private String idioma;
+
+    public MensajeChatDTO() {
+        
+    }
+    
+    public MensajeChatDTO(Integer usuarioID, String nombre, String contenido, LocalDateTime fechaHora, String idioma) {
+    this.usuarioID = usuarioID;
+    this.nombre = nombre;
+    this.contenido = contenido;
+    this.fechaHora = fechaHora;
+    this.idioma = idioma;
+    }
+
+
+    public Integer getUsuarioID() {
+        return usuarioID;
+    }
+    public void setUsuarioID(Integer usuarioID) {
+        this.usuarioID = usuarioID;
+    }
+    public String getNombre() {
+        return nombre;
+    }
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+    public String getContenido() {
+        return contenido;
+    }
+    public void setContenido(String contenido) {
+        this.contenido = contenido;
+    }
+    public LocalDateTime getFechaHora() {
+        return fechaHora;
+    }
+    public void setFechaHora(LocalDateTime fechaHora) {
+        this.fechaHora = fechaHora;
+    }
+    public String getIdioma() {
+        return idioma;
+    }
+    public void setIdioma(String idioma) {
+        this.idioma = idioma;
+    }
+
+    
+}
