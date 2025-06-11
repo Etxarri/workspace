01454d817239ee96b4a1d916d3bcaeddbdb3bf85
Mikel Etxarri <mikel.echarri@alumni.mondragon.edu>
2025-06-11 09:23:22 +0200

    backup

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -1,4 +1,23 @@
 #!/bin/bash
+# Pasos para automatizar este proceso:
+# 1. Instalar cron
+# sudo apt update
+# sudo apt install cron
+
+# 2. Arranca el servicio
+# sudo systemctl enable cron
+# sudo systemctl start cron
+
+# 3. Editar las tareas programadas
+# crontab -e
+
+# 4. Agregar esta linea para que a las 3:00 de la mañana haga un backup
+# 0 3 * * * /workspace/mysql_backup/mysql_backup.sh >> /workspace/mysql_backup/mysql_backup.log 2>&1
+
+# 5. Darle permisos al script y a la carpeta de backups
+# chmod +x /workspace/mysql_backup/mysql_backup.sh
+# chmod 700 /workspace/mysql_backup/backups
+
 
 # Configura tus datos
 USER="root"
@@ -11,4 +30,3 @@ FILENAME="$OUTPUT_DIR/backup_$DATABASE_$FECHA.sql"
 # Crear backup
 mysqldump -h 172.18.0.2 -u $USER -p$PASSWORD $DATABASE > $FILENAME
 
-# 0 3 * * * /workspace/mysql_backup/mysql_backup.sh >> /workspace/mysql_backup/mysql_backup.log 2>&1
