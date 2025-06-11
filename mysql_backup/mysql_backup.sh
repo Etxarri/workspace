a9a6881d94b7e080c1684b6e71c4177586778b21
Mikel Etxarri <mikel.echarri@alumni.mondragon.edu>
2025-06-11 11:09:20 +0200

    backup bien

--- mysql_backup/mysql_backup.sh
+++ mysql_backup/mysql_backup.sh
@@ -1,4 +1,16 @@
 #!/bin/bash
+
+# Configura tus datos
+USER="root"
+PASSWORD="pasahitza"
+DATABASE="welco"
+OUTPUT_DIR="/workspace/mysql_backup/backups"
+FECHA=$(date +\%Y-\%m-\%d)
+FILENAME="$OUTPUT_DIR/backup_$DATABASE_$FECHA.sql"
+
+# Crear backup
+mysqldump -h 172.18.0.2 -u $USER -p$PASSWORD $DATABASE > $FILENAME
+
 # Pasos para automatizar este proceso:
 # 1. Instalar cron
 # sudo apt update
@@ -11,22 +23,13 @@
 # 3. Editar las tareas programadas
 # crontab -e
 
-# 4. Agregar esta linea para que a las 3:00 de la mañana haga un backup
-# 0 3 * * * /workspace/mysql_backup/mysql_backup.sh >> /workspace/mysql_backup/mysql_backup.log 2>&1
+# 4. Agregar esta linea para que a las 18:00 de la mañana haga un backup
+# 0 18 * * * /workspace/mysql_backup/mysql_backup.sh >> /workspace/mysql_backup/mysql_backup.log 2>&1
 
 # 5. Darle permisos al script y a la carpeta de backups
-# chmod +x /workspace/mysql_backup/mysql_backup.sh
-# chmod 700 /workspace/mysql_backup/backups
+# sudo chmod +x /workspace/mysql_backup/mysql_backup.sh
+# sudo chmod 700 /workspace/mysql_backup/backups
 
 
-# Configura tus datos
-USER="root"
-PASSWORD="pasahitza"
-DATABASE="welco"
-OUTPUT_DIR="/workspace/mysql_backup/backups"
-FECHA=$(date +\%Y-\%m-\%d)
-FILENAME="$OUTPUT_DIR/backup_$DATABASE_$FECHA.sql"
 
-# Crear backup
-mysqldump -h 172.18.0.2 -u $USER -p$PASSWORD $DATABASE > $FILENAME
 
